8e07e4a73e0fd5e7616455c69d5f0102780f7a59
Mehdi Benadda <me@mbenadda.com>
2018-04-21 01:52:30 +0200

    🔥config) remove config related to tracking scripts
    Remove 2 extensions that are used to add tracking scripts to the
    pages (Twitter's and Google Analytics) from package.json and from
    gatsby-config.
    Remove a reference to GA in siteConfig.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -16,7 +16,6 @@ module.exports = {
   siteRssAuthor: "Mehdi Benadda", // The author name used in the RSS file
   // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
   sitePaginationLimit: 10, // The max number of posts per page.
-  googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
   // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
   siteSocialUrls: [
     "https://github.com/mbenadda",
